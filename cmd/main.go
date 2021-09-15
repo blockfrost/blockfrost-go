94e1dd188051a4e1134c8db141a6ca87729fda14
kenriortega <kenriortega@gmail.com>
2021-09-15 19:55:55 -0400

    refactor: split all method and types on an specific type

--- cmd/main.go
+++ cmd/main.go
@@ -6,12 +6,16 @@ import (
 	"log"
 	"os"
 
-	blockfrostgo "github.com/blockfrost/blockfrost-go"
+	blockfrostgo "github.com/blockfrost/blockfrost-go/pkg/api"
+)
+
+const (
+	stakeAddrTestnet = "stake_test1uzaghtuxs0z569hnc68enjpnzy5tarqeg54k9p6rj5jaakq4dwqjg"
 )
 
 func main() {
 
-	client := blockfrostgo.NewClient(
+	client := blockfrostgo.NewBlockfrostAPI(
 		os.Getenv("API_KEY"),
 		blockfrostgo.CardanoTestnet,
 		false,
@@ -19,36 +23,45 @@ func main() {
 		os.Stdout,
 	)
 
-	appinfo, err := client.Info(context.TODO())
+	// appinfo, err := client.Info(context.TODO())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(appinfo)
+
+	// health, err := client.Health(context.TODO())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(health.IsHealthy)
+
+	// healthClock, err := client.HealthClock(context.TODO())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(healthClock.ServerTime)
+
+	// metrics, err := client.Metrics(context.TODO())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(metrics)
+	// metricsEndpoints, err := client.MetricsEndpoints(context.TODO())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(metricsEndpoints)
+
+	accountAddr, err := client.Account(
+		context.Background(),
+		stakeAddrTestnet,
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(appinfo)
-
-	health, err := client.Health(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(health.IsHealthy)
-
-	healthClock, err := client.HealthClock(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(healthClock.ServerTime)
-
-	metrics, err := client.Metrics(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(metrics)
-	metricsEndpoints, err := client.MetricsEndpoints(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(metricsEndpoints)
+	fmt.Println(accountAddr)
 }
---
+++ pkg/api/health.go
@@ -0,0 +1,110 @@
+package blockfrostgo
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+// AppInfo return the Root endpoint
+// has no other function than to point end users to documentation.
+type AppInfo struct {
+	URL     string `json:"url,omitempty"`
+	Version string `json:"version,omitempty"`
+}
+
+// HealthStatus return the Backend health status
+// Return backend status as a boolean. Your application
+// should handle situations when backend for the given chain is unavailable.
+type HealthStatus struct {
+	IsHealthy bool `json:"is_healthy,omitempty"`
+}
+
+// HealthClock return the Current backend time
+// This endpoint provides the current UNIX time.
+// Your application might use this to verify
+// if the client clock is not out of sync.
+type HealthClock struct {
+	ServerTime int64 `json:"server_time,omitempty"`
+}
+
+func (c *client) Info(ctx context.Context) (AppInfo, error) {
+	requestUrl, err := url.Parse(c.url)
+	if err != nil {
+		return AppInfo{}, err
+	}
+
+	status, res, err := c.apiCall(
+		ctx,
+		http.MethodGet,
+		requestUrl.String(),
+		nil,
+	)
+	if err != nil {
+		return AppInfo{}, err
+	}
+	if status != http.StatusOK {
+		return AppInfo{}, fmt.Errorf("unexpected response status %d: %q", status, res)
+	}
+	result := AppInfo{}
+	err = json.NewDecoder(strings.NewReader(res)).Decode(&result)
+	if err != nil {
+		return AppInfo{}, fmt.Errorf("decoding error for data %s: %v", res, err)
+	}
+	return result, nil
+}
+
+func (c *client) Health(ctx context.Context) (HealthStatus, error) {
+	requestUrl, err := url.Parse(fmt.Sprintf("%s/%s", c.url, healthStatusPath))
+	if err != nil {
+		return HealthStatus{}, err
+	}
+
+	status, res, err := c.apiCall(
+		ctx,
+		http.MethodGet,
+		requestUrl.String(),
+		nil,
+	)
+	if err != nil {
+		return HealthStatus{}, err
+	}
+	if status != http.StatusOK {
+		return HealthStatus{}, fmt.Errorf("unexpected response status %d: %q", status, res)
+	}
+	result := HealthStatus{}
+	err = json.NewDecoder(strings.NewReader(res)).Decode(&result)
+	if err != nil {
+		return HealthStatus{}, fmt.Errorf("decoding error for data %s: %v", res, err)
+	}
+	return result, nil
+}
+
+func (c *client) HealthClock(ctx context.Context) (HealthClock, error) {
+	requestUrl, err := url.Parse(fmt.Sprintf("%s/%s", c.url, healthClockPath))
+	if err != nil {
+		return HealthClock{}, err
+	}
+
+	status, res, err := c.apiCall(
+		ctx,
+		http.MethodGet,
+		requestUrl.String(),
+		nil,
+	)
+	if err != nil {
+		return HealthClock{}, err
+	}
+	if status != http.StatusOK {
+		return HealthClock{}, fmt.Errorf("unexpected response status %d: %q", status, res)
+	}
+	result := HealthClock{}
+	err = json.NewDecoder(strings.NewReader(res)).Decode(&result)
+	if err != nil {
+		return HealthClock{}, fmt.Errorf("decoding error for data %s: %v", res, err)
+	}
+	return result, nil
+}
