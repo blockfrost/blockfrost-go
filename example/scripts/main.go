3abccb0b49873953a7d50f9478222ac5657912db
Hannes <hanneskimara@gmail.com>
2021-09-20 20:07:19 +0300

    docs: add example file

---
+++ example/scripts/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/blockfrost/blockfrost-go"
+)
+
+func main() {
+	api, err := blockfrost.NewAPIClient(blockfrost.APIClientOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scripts, err := api.Scripts(context.TODO(), blockfrost.APIPagingParams{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(scripts) == 0 {
+		return
+	}
+
+	addr := scripts[0].ScriptHash
+
+	script, err := api.Script(context.TODO(), addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Script: %+v", script)
+
+}
