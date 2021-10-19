fd2ce1aa6e26e61edce6805a2d4553f6989543ba
Hannes <hanneskimara@gmail.com>
2021-10-19 08:43:23 +0300

    docs+ref: add docs for genesis method
    enforce required fields

--- api_ledger.go
+++ api_ledger.go
@@ -12,19 +12,21 @@ var (
 	resourceGenesis = "genesis"
 )
 
+// GenesisBlock contains the information of the genesis block of the network.
 type GenesisBlock struct {
-	ActiveSlotsCoefficient float32 `json:"active_slots_coefficient,omitempty"`
-	UpdateQuorum           float32 `json:"update_quorum,omitempty"`
-	MaxLovelaceSupply      string  `json:"max_lovelace_supply,omitempty"`
-	NetworkMagic           int     `json:"network_magic,omitempty"`
-	EpochLength            int     `json:"epoch_length,omitempty"`
-	SystemStart            int     `json:"system_start,omitempty"`
-	SlotsPerKesPeriod      int     `json:"slots_per_kes_period,omitempty"`
-	SlotLength             int     `json:"slot_length,omitempty"`
-	MaxKesEvolutions       int     `json:"max_kes_evolutions,omitempty"`
-	SecurityParam          int     `json:"security_param,omitempty"`
+	ActiveSlotsCoefficient float32 `json:"active_slots_coefficient"`
+	UpdateQuorum           float32 `json:"update_quorum"`
+	MaxLovelaceSupply      string  `json:"max_lovelace_supply"`
+	NetworkMagic           int     `json:"network_magic"`
+	EpochLength            int     `json:"epoch_length"`
+	SystemStart            int     `json:"system_start"`
+	SlotsPerKesPeriod      int     `json:"slots_per_kes_period"`
+	SlotLength             int     `json:"slot_length"`
+	MaxKesEvolutions       int     `json:"max_kes_evolutions"`
+	SecurityParam          int     `json:"security_param"`
 }
 
+// Genesis returns the information about blockchain genesis.
 func (c *apiClient) Genesis(ctx context.Context) (gen GenesisBlock, err error) {
 	requestUrl, err := url.Parse(fmt.Sprintf("%s/%s", c.server, resourceGenesis))
 	if err != nil {
